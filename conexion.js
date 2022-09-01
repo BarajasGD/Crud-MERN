65ff7f876e19417aeff380b84ff48adb5ca57fa5
DanielBarajas193047 <al193047@alumnos.uacj.mx>
2022-08-31 22:23:41 -0600

    make it better

--- conexion.js
+++ conexion.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/crudmern');
-
+//mongoose.connect('mongodb://127.0.0.1:27017/crudmern');
+mongoose.connect('mongodb+srv://SrRetro:054503reymist.@cluster0.juqyqem.mongodb.net/?retryWrites=true&w=majority');
 const objetobd = mongoose.connection
 
 objetobd.on('connected', ()=>{console.log('Conexion correcta a MongoDB')})
